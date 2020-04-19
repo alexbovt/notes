7f2b014e86b38cea6e18c5807965fe4e6acf3ab0
alexbovt <bovttt@gmail.com>
2020-04-19 23:11:08 +0200

    import small fix

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-
 import { Provider } from 'react-redux'
-import { appStore } from './app.store'
 
+import { appStore } from './app.store'
 import { Main } from './main'
 
 export const App = (): JSX.Element => (
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Hello } from './hello/hello'
 import { Todos } from './todo/todo.component'
