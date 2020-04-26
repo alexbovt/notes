c4c2b914919bdc41d55f21a1f1374ecf0a88233f
alexbovt <bovttt@gmail.com>
2020-04-26 21:44:21 +0200

    move prettier config

--- client/src/app.reducer.ts
+++ client/src/app.reducer.ts
@@ -1,9 +1,9 @@
-import {combineReducers} from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit'
 
-import todosSlice from './todo/todos.slice';
+import todosSlice from './todo/todos.slice'
 
 export const appReducer = combineReducers({
-    todo: todosSlice.reducer
-});
+  todo: todosSlice.reducer,
+})
 
 export type ApplicationState = ReturnType<typeof appReducer>
